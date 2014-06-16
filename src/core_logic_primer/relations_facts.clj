2b676c0b18f4e7d816ff223d75fa8b3d8869c681
Khalid Jebbari <khalid.jebbari@externe.sacem.fr>
2014-06-16 18:47:52 +0200

    Add more relations problems, with comments too

--- src/core_logic_primer/relations_facts.clj
+++ src/core_logic_primer/relations_facts.clj
@@ -1,11 +1,11 @@
-(ns enigmes-logiques
+(ns relations-facts
   (:require [clojure.core.logic :as l])
   (:require [clojure.core.logic.pldb :as pldb]))
 
 (pldb/db-rel father Father Child)
 (pldb/db-rel mother Mother Child)
 
-(def fathers (pldb/db [father 'Vito 'Michael]
+(def parents (pldb/db [father 'Vito 'Michael]
                       [father 'Vito 'Sonny]
                       [father 'Vito 'Fredo]
                       [father 'Michael 'Anthony]
@@ -14,9 +14,9 @@
                       [father 'Sonny 'Francesca]
                       [father 'Sonny 'Kathryn]
                       [father 'Sonny 'Frank]
-                      [father 'Sonny 'Santino]))
+                      [father 'Sonny 'Santino]
 
-(def mothers (pldb/db [mother 'Carmela 'Michael]
+                      [mother 'Carmela 'Michael]
                       [mother 'Carmela 'Sonny]
                       [mother 'Carmela 'Fredo]
                       [mother 'Kay 'Mary]
@@ -26,10 +26,53 @@
                       [mother 'Sandra 'Frank]
                       [mother 'Sandra 'Santino]))
 
-(pldb/with-db fathers
+;; Qui sont les enfants de Vito ?
+
+(pldb/with-db parents
   (l/run* [q]
     (father 'Vito q)))
 
-(pldb/with-db mothers
+;; Qui est la mère de Frank ?
+
+(pldb/with-db parents
   (l/run* [q]
     (mother q 'Frank)))
+
+;; Définir une relation parent :
+;; Un parent est un père OU une mère
+
+(defn parento [p child]
+  (l/conde
+   [(father p child)]
+   [(mother p child)]))
+
+;; Qui sont les parents de Michael ?
+
+(pldb/with-db parents
+  (l/run* [q]
+        (parent q 'Michael)))
+
+;; Définir une relation grand-parent :
+;; Un grand-parent est le parent du parent d'un enfant !
+
+(defn grand-parento [gparent child]
+  (l/fresh [q]
+       (parent gparent q)
+       (parent q child)))
+
+;; Qui sont les petits enfants de Vito ?
+
+(pldb/with-db parents
+  (l/run* [q]
+      (grand-parent 'Vito q)))
+
+;; Trouvez les couples distincts de parents
+
+(distinct (pldb/with-db parents
+  (l/run* [q]
+      (l/fresh [p m c]
+           (l/== q [p m])
+           (mother m c)
+           (father p c)))))
+
+
